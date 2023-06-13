0744ca71eddf83ff937052c88b6d06482a7d84d0
Igor Rudakov <igor.rudakov@integrio.net>
2023-06-13 12:18:11 +0300

    Implemented MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://igorxlib:tLdC0CSHELbfHxl3@cluster0.kybkh9y.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
